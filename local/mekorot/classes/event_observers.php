e2259cf522185b4af130a7fa5840a94d15411e93
Noam Leibovitch <noamleib@gmail.com>
2020-11-08 13:58:26 +0200

    Added SAP D to completion call

--- local/mekorot/classes/event_observers.php
+++ local/mekorot/classes/event_observers.php
@@ -33,81 +33,81 @@ namespace local_mekorot;
 
 class event_observers {
 
-	    /**
-	    *
-	    *  @param \core\event\course_completed $event .
-	    */
-	public static function agent_log($debug){
-		file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($debug), FILE_APPEND);
-		}
-	public static function course_completed($event) {
-		global $CFG, $DB;
-		\local_mekorot\event_observers::agent_log($event);
-		\local_mekorot\event_observers::agent_log("User comleted a course event fired");
-        	$eventdata = $event->get_data();
-        	//Retrieve the course id and user id from the eventdata objects
-		$lmsid = $eventdata['relateduserid'];
-                $courseid = $eventdata['courseid'];
-		$user = \core_user::get_user($lmsid);
-		$user = json_decode(json_encode($user), true);
-		$employeeId = $user['idnumber'];
-		$group = groups_get_user_groups($courseid, $lmsid);
-		$group = json_decode(json_encode($group), true);
-		$groupId = json_encode($group[0][0]);
-		$E = groups_get_group($groupId);
-		$E = $E->idnumber;
-		$grade= grade_get_course_grade($lmsid, $courseid);
-		$grade= $grade->str_grade;
-		\local_mekorot\event_observers::agent_log ("grade is: " . $grade);
-	    	\local_mekorot\event_observers::agent_log ('lms User Id: '.$lmsid);
-		\local_mekorot\event_observers::agent_log ('lms Course Id: '.$courseid);
-		\local_mekorot\event_observers::agent_log ('Employee ID: '.$employeeId);
-		\local_mekorot\event_observers::agent_log ('SAP E: '.$E);
-		
-$postfields = array('employeeId'=>$employeeId, 'sapE'=>$E, 'grade'=>$grade);
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://noam.requestcatcher.com/test');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_POST, 1);
+    /**
+     *
+     *  @param \core\event\course_completed $event .
+     */
+    public static function agent_log($debug){
+        file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($debug), FILE_APPEND);
+    }
+    public static function course_completed($event) {
+        global $CFG, $DB;
+        \local_mekorot\event_observers::agent_log(" *************** User comleted a course event fired, Updating SAP ***************");
+        $eventdata = $event->get_data();
+        //Retrieve the course id and user id from the eventdata objects
+        $lmsid = $eventdata['relateduserid'];
+        $courseid = $eventdata['courseid'];
+        $course = get_course($courseid);
+        $D = $course->idnumber;
+        $user = \core_user::get_user($lmsid);
+        $user = json_decode(json_encode($user), true);
+        $employeeId = $user['idnumber'];
+        $group = groups_get_user_groups($courseid, $lmsid);
+        $group = json_decode(json_encode($group), true);
+        $groupId = json_encode($group[0][0]);
+        $E = groups_get_group($groupId);
+        $E = $E->idnumber;
+        $grade= grade_get_course_grade($lmsid, $courseid);
+        $grade= $grade->str_grade;
+        \local_mekorot\event_observers::agent_log ("updating SAP that lms User ".$lmsid." completed course: ".$courseid." with grade: " . $grade);
+        \local_mekorot\event_observers::agent_log ('SAP Employee ID: '.$employeeId .', SAP E: '.$E. ', SAP D: '.$D);
+
+        $postfields = array('employeeId'=>$employeeId,'sapD'=>$D, 'sapE'=>$E, 'grade'=>$grade);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://noam.requestcatcher.com/test');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
 // Edit: prior variable $postFields should be $postfields;
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-$result = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+        $result = curl_exec($ch);
+        \local_mekorot\event_observers::agent_log ("SAP Response: ");
+        \local_mekorot\event_observers::agent_log (json_encode($result));
+        \local_mekorot\event_observers::agent_log(" *************** User comleted a course event finished ***************");
 
-		\local_mekorot\event_observers::agent_log (json_encode($result));
-		}
-	
+    }
 
-public static function course_completion_updated($event) {
-                global $CFG, $DB;
-                \local_mekorot\event_observers::agent_log($event);
-                \local_mekorot\event_observers::agent_log("course__completion__updated event fired");
-                $eventdata = $event->get_data();
-                //Retrieve the course id and user id from the eventdata objects
-                $userid = $eventdata['userid'];
-                $courseid = $eventdata['courseid'];
-		\local_mekorot\event_observers::agent_log($userid);
-		\local_mekorot\event_observers::agent_log($courseid);
 
-		if ($entry = $DB->get_record('local_iomad_track', array('userid' => $userid,
-                                                                'courseid' => $courseid,
-								'timestarted' => null,
-                                                                'timecompleted' => null))) {
-        	file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($entry), FILE_APPEND);
-            	$entry = json_decode(json_encode($entry), true);
+    public static function course_completion_updated($event) {
+        /*global $CFG, $DB;
+        \local_mekorot\event_observers::agent_log($event);
+        \local_mekorot\event_observers::agent_log("course__completion__updated event fired");
+        $eventdata = $event->get_data();
+        //Retrieve the course id and user id from the eventdata objects
+        $userid = $eventdata['userid'];
+        $courseid = $eventdata['courseid'];
+\local_mekorot\event_observers::agent_log($userid);
+\local_mekorot\event_observers::agent_log($courseid);
+
+if ($entry = $DB->get_record('local_iomad_track', array('userid' => $userid,
+                                                        'courseid' => $courseid,
+                        'timestarted' => null,
+                                                        'timecompleted' => null))) {
+    file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($entry), FILE_APPEND);
+        $entry = json_decode(json_encode($entry), true);
 //		$entry = json_encode($entry);
-		// We already have an entry.  Change the issue time.		
-            	$timestarted = strtotime('now');
-		file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($timestarted), FILE_APPEND);
-		file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($entry), FILE_APPEND);
-		$DB->set_field('local_iomad_track', 'timestarted', $timestarted, array('id' => $entry['id']));
-		$DB->set_field('local_iomad_track', 'modifiedtime', $timestarted, array('id' => $entry['id']));
+// We already have an entry.  Change the issue time.
+        $timestarted = strtotime('now');
+file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($timestarted), FILE_APPEND);
+file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($entry), FILE_APPEND);
+$DB->set_field('local_iomad_track', 'timestarted', $timestarted, array('id' => $entry['id']));
+$DB->set_field('local_iomad_track', 'modifiedtime', $timestarted, array('id' => $entry['id']));
 //            	file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($DB->update_record('local_iomad_track', array('id' => $entry['id'], 'timestarted' => $timestarted))), FILE_APPEND);
-		file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($DB->get_record('local_iomad_track', array('userid' => $userid,
-                                                                'courseid' => $courseid,
-                                                                'timecompleted' => null))), FILE_APPEND);
-        }
+file_put_contents ('/var/www/html/log.txt',PHP_EOL .date('D, d M Y H:i:s'). serialize($DB->get_record('local_iomad_track', array('userid' => $userid,
+                                                        'courseid' => $courseid,
+                                                        'timecompleted' => null))), FILE_APPEND);
 }
+*/}
 }
